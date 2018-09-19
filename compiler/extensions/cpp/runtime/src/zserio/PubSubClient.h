04f13bace503b22ebea7e6ceb1a151a8b004ee95
Serein Pfeiffer <s.pfeiffer@klebert-engineering.de>
2018-09-19 17:14:59 +0200

    PubSub: Unify Publisher and Subscriber to get rid of redundant code and make PubSub system easier to use/understand.

---
+++ compiler/extensions/cpp/runtime/src/zserio/PubSubClient.h
@@ -0,0 +1,53 @@
+#ifndef ZSERIO_PUB_SUB_SYSTEM_H_INC
+#define ZSERIO_PUB_SUB_SYSTEM_H_INC 
+
+#include <string>
+#include <memory>
+
+namespace zserio 
+{
+
+class Topic;
+
+/** Represents the PubSubClient of zserio which can be used with different implementations
+    Mosquitto MQTT broker ...  */
+class PubSubClient
+{
+public:
+    using SubscriptionId = size_t;
+
+    PubSubClient();
+    virtual ~PubSubClient();
+
+    PubSubClient(const PubSubClient& pubSub) = delete;
+    PubSubClient& operator=(const PubSubClient& pubSub) = delete;
+    PubSubClient(PubSubClient&&) = delete;
+    PubSubClient& operator=(PubSubClient&&) = delete;
+    
+    struct HostInformation
+    {
+        std::string hostname;
+        uint32_t	port;
+        uint32_t	keepalive;
+        bool		clean_session;
+        std::string client_id;
+    };
+
+    /** Tries to connect ot the specified host. */
+    void connect(const PubSubClient::HostInformation& host);
+
+    /** Publishes data using the specified topic */
+    void publish(const std::string &topic, const uint8_t* buffer, size_t size, int qos, bool retain);
+
+    /* Subscribe to the specified topic */
+    void subscribe(const Topic &topic);
+
+    /* Unsubscribe from specified topic */
+    void unsubscribe(const Topic &topic);
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> m_impl;
+};
+}
+#endif
