310705102508515a8a164de99ec153f73cf1819f
Serein Pfeiffer <s.pfeiffer@klebert-engineering.de>
2019-02-28 17:25:36 +0100

    Template concept: Reflect that NativeUnboundTemplateType have an empty/no namespace.

--- compiler/extensions/cpp/src/zserio/emit/cpp/types/NativeUnboundTemplateType.java
+++ compiler/extensions/cpp/src/zserio/emit/cpp/types/NativeUnboundTemplateType.java
@@ -4,8 +4,8 @@ import zserio.ast.PackageName;
 public class NativeUnboundTemplateType extends CppNativeType {
     public NativeUnboundTemplateType(String name)
     {
-        super(STD_PACKAGE_NAME, name, false);
+        super(NO_PACKAGE_NAME, name, false);
     }
 
-    private static final PackageName STD_PACKAGE_NAME = new PackageName.Builder().addId("std").get();
+    private static final PackageName NO_PACKAGE_NAME = new PackageName.Builder().addId("").get();
 }
