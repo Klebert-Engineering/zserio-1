588c828d8fc9a18da60825bf865bc027e342e0e8
Joseph Birkner <j.birkner@klebert-engineering.de>
2019-03-22 19:17:00 +0100

    Finished basic UrlClient impl. w/ vector<uint8_t> interface.

--- compiler/extensions/cpp/runtime/src/zserio/UrlClient.cpp
+++ compiler/extensions/cpp/runtime/src/zserio/UrlClient.cpp
@@ -1,13 +1,25 @@
 #include "UrlClient.h"
 
 #include <curl/curl.h>
+#include <iostream>
 
 namespace zserio
 {
 
+UrlClient::~UrlClient() = default;
+
 struct UrlClient::Impl
 {
     CURL *curl = nullptr;
+    CURLcode status = CURLE_OK;
+    std::string statusText;
+    std::vector<uint8_t> result;
+
+    static size_t curlRecvFn (void *ptr, size_t size, size_t nmemb, void *thisPtr) {
+        auto& resultVector = reinterpret_cast<Impl*>(thisPtr)->result;
+        resultVector.insert(resultVector.end(), (uint8_t*)ptr, (uint8_t*)ptr + nmemb);
+        return nmemb;
+    };
 
     Impl() {
         curl = curl_easy_init();
@@ -22,10 +34,35 @@ struct UrlClient::Impl
 
 UrlClient::UrlClient() : impl_(new Impl) {}
 
-std::pair<const uint8_t*, size_t> UrlClient::fetchResource(const std::string& uri) {
-    curl_easy_setopt(impl_->curl, CURLOPT_URL, uri.c_str());
+std::vector<uint8_t> UrlClient::fetchResource(const std::string& uri)
+{
+    impl_->result.clear();
 
-    return {nullptr, 0};
+    curl_easy_setopt(impl_->curl, CURLOPT_URL, uri.c_str());
+    curl_easy_setopt(impl_->curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(impl_->curl, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(impl_->curl, CURLOPT_WRITEFUNCTION, &Impl::curlRecvFn);
+    curl_easy_setopt(impl_->curl, CURLOPT_WRITEDATA, impl_.get());
+
+    impl_->status = curl_easy_perform(impl_->curl);
+    if (impl_->status != CURLE_OK) {
+        impl_->statusText = std::string(curl_easy_strerror(impl_->status));
+    }
+
+    return impl_->result;
+}
+
+UrlClient::Status UrlClient::status() {
+    switch (impl_->status) {
+    case CURLE_OK:
+        return Healthy;
+    default:
+        return Error;
+    }
+}
+
+std::string UrlClient::statusDescription() {
+    return impl_->statusText;
 }
 
 }
